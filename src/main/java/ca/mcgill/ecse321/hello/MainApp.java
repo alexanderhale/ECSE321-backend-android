b38bce04bdd8bcd78265598550a816c240f6bf76
eozer <ege.ozer@mail.mcgill.ca>
2018-09-26 13:15:41 -0400

    Added JPA Hibernate support with MySql connector Updated MainApp Removed Unnecessary files You can now run the project on the web

--- src/main/java/ca/mcgill/ecse321/hello/MainApp.java
+++ src/main/java/ca/mcgill/ecse321/hello/MainApp.java
@@ -1,13 +1,32 @@
 package ca.mcgill.ecse321.hello;
+import ca.mcgill.ecse321.model.User;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
+@RestController
+@SpringBootApplication
 public class MainApp {
-	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("SpringBeans.xml");
 
-		HelloWorld hi = (HelloWorld) context.getBean("helloBean");
-		hi.sayHi();
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MainApp.class, args);
+
+
+    }
+
+    @RequestMapping("/")
+    public String greeting(){
+
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("persistenceUnit");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        User user = entityManager.find(User.class, 1);
+
+        return user.getUsername();
+    }
 }
