3347d438ba34053bc3cf2571603ed36b40a8ec80
eozer <ege.ozer@mail.mcgill.ca>
2018-09-30 21:46:16 -0400

    -Switched database from MySql to PostresSQL -Switched from local to Heroku's cloud PostgresSQL server -Added username and password into the database with our names

--- src/main/java/ca/mcgill/ecse321/hello/MainApp.java
+++ src/main/java/ca/mcgill/ecse321/hello/MainApp.java
@@ -1,4 +1,5 @@
 package ca.mcgill.ecse321.hello;
+
 import ca.mcgill.ecse321.model.User;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.util.List;
 
 @RestController
 @SpringBootApplication
@@ -16,17 +18,27 @@ public class MainApp {
     public static void main(String[] args) {
         SpringApplication.run(MainApp.class, args);
 
-
     }
 
     @RequestMapping("/")
-    public String greeting(){
+    public String greeting() {
 
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("persistenceUnit");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
 
-        User user = entityManager.find(User.class, 0);
+        String temp = "";
 
-        return user.getUsername();
+        List<User> listUsernames = entityManager.createQuery(
+                "SELECT p FROM User p").getResultList();
+
+        if (listUsernames == null) {
+            temp = "no usernames found";
+        } else {
+            for (User user : listUsernames) {
+                temp += user.getUsername() + " ";
+            }
+        }
+
+        return temp;
     }
 }
