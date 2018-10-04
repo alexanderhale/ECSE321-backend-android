65f0e89acfa0069bd00bc05588448cf073bb5c81
Walid Chabchoub <walid.chabchoub@mail.mcgill.ca>
2018-10-04 18:07:42 -0400

    Started integrating token for log-in persistence with JWT

--- src/main/java/ca/mcgill/ecse321/MainApp.java
+++ src/main/java/ca/mcgill/ecse321/MainApp.java
@@ -2,9 +2,21 @@ package ca.mcgill.ecse321;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import ca.mcgill.ecse321.config.JwtFilter;
 
 @SpringBootApplication()
 public class MainApp {
+
+    @Bean
+    public FilterRegistrationBean jwtFilter() {
+        final FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        registrationBean.setFilter(new JwtFilter());
+        registrationBean.addUrlPatterns("/secure/*");
+
+        return registrationBean;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MainApp.class, args);
--- src/main/java/ca/mcgill/ecse321/user/UserController.java
+++ src/main/java/ca/mcgill/ecse321/user/UserController.java
@@ -4,20 +4,18 @@ import ca.mcgill.ecse321.api.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import javax.servlet.ServletException;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.util.Date;
 
 @RestController
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
-
-    @GetMapping("/users")
-    public ResponseEntity getAllUsers() {
-        List<User> users = userRepository.findAll();
-        return ResponseEntity.status(HttpStatus.OK).body(users);
-    }
 
     @PostMapping("/register")
     public ResponseEntity registerUser(@RequestBody User newUser) {
@@ -26,5 +24,35 @@ public class UserController {
         }
         userRepository.save(newUser);
         return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse(200, "User successfully created"));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity loginUser(@RequestBody User login) throws ServletException {
+
+        String jwtToken = "";
+
+        if (login.getUsername() == null || login.getPassword() == null) {
+            throw new ServletException("Please fill in username and password");
+        }
+
+        String username = login.getUsername();
+        String password = login.getPassword();
+
+        User user = userRepository.findUserByUsername(username).get(0);
+
+        if (user == null) {
+            throw new ServletException("User not found.");
+        }
+
+        String pwd = user.getPassword();
+
+        if (!password.equals(pwd)) {
+            throw new ServletException("Invalid login. Please check your username and password.");
+        }
+
+        jwtToken = Jwts.builder().setSubject(username).claim("roles", "user").setIssuedAt(new Date())
+                .signWith(SignatureAlgorithm.HS256, "elonmusk").compact(); //setting "elonmusk" as our signing key for tokens
+        return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse(200, jwtToken));
+
     }
 }
