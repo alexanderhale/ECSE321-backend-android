88d369976fd077e8abc929dbf760b322d2248bcb
SpencerHandfield <spencer.handfield@mail.mcgill.ca>
2018-10-07 00:30:15 -0400

    tried adding test/basic junit test and importing apache lang commons

--- src/test/java/ca.mcgill.ecse321/ApiTest.java
+++ src/test/java/ca.mcgill.ecse321/ApiTest.java
@@ -1,16 +1,12 @@
 package ca.mcgill.ecse321;
 
-import org.apache.http.HttpEntity;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpUriRequest;
-import org.junit.Test;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.TestPropertySource;
+import org.junit.Test;
 
 import java.io.IOException;
 
@@ -36,19 +32,34 @@ public class ApiTest {
         assertEquals(httpResponse.getStatusLine().getStatusCode(), HttpStatus.SC_NOT_FOUND);
     }
     @Test
-    public void testRiderIsNull(){
-/*        HttpUriRequest request = new HttpGet("https://ecse321-project.herokuapp.com/rider");
+    public void testRiderIsNull() throws IOException {
+        HttpUriRequest request = new HttpGet("https://ecse321-project.herokuapp.com/rider");
 
         HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
 
-        assertEquals(httpResponse.getStatusLine().getStatusCode(), HttpStatus.SC_NOT_FOUND);*/
+        assertEquals(httpResponse.getStatusLine().getStatusCode(), HttpStatus.SC_NOT_FOUND);
     }
 
     @Test
     public void testDriverLogin() throws Exception{
 
     }
-
+    @Test
+    public void testDriverLogin_driverNotFound() throws Exception {
+        String testName = RandomStringUtils.randomAlphabetic(7);
+        System.out.println(testName);
+        HttpUriRequest request = new HttpGet("https://ecse321-project.herokuapp.com/driver" + testName);
+        HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
+        assertEquals(httpResponse.getStatusLine().getStatusCode(), HttpStatus.SC_NOT_FOUND);
+    }
+    @Test
+    public void testRiderLogin_driverNotFound() throws Exception {
+        String testName = RandomStringUtils.randomAlphabetic(7);
+        System.out.println(testName);
+        HttpUriRequest request = new HttpGet("https://ecse321-project.herokuapp.com/rider" + testName);
+        HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
+        assertEquals(httpResponse.getStatusLine().getStatusCode(), HttpStatus.SC_NOT_FOUND);
+    }
     @Test
     public void testRegisterSuccessfully() throws Exception {
 
