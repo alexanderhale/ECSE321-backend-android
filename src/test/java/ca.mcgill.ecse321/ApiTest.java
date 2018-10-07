4e42bedf97726ad37d7ea82e13b594e9f09dde0e
SpencerHandfield <spencer.handfield@mail.mcgill.ca>
2018-10-07 02:43:49 -0400

    added just json test and unfinished login test idea

--- src/test/java/ca.mcgill.ecse321/ApiTest.java
+++ src/test/java/ca.mcgill.ecse321/ApiTest.java
@@ -5,6 +5,7 @@ import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.entity.ContentType;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.junit.Test;
 
@@ -41,9 +42,39 @@ public class ApiTest {
     }
 
     @Test
-    public void testDriverLogin() throws Exception{
-
+    public void mediaTypeIsCorrectTest() throws Exception {
+        String jSonType = "application/json";
+        HttpUriRequest jSonRequest = new HttpGet("https://ecse321-project.herokuapp.com");
+        HttpResponse jSonResponse = HttpClientBuilder.create().build().execute(jSonRequest);
+        String type = ContentType.getOrDefault(jSonResponse.getEntity()).getMimeType();
+        assertEquals (jSonType, type);
     }
+
+//not sure how to test proper print for logging in, will probably scrap and focus on http methods
+//    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+//    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+//    private final PrintStream originalOut = System.out;
+//    private final PrintStream originalErr = System.out;
+//    @Before
+//    public void createStreams() {
+//        //create the streams we'll use to test
+//        System.setOut(new PrintStream(outContent));
+//        System.setErr(new PrintStream(errContent));
+//    }
+//    @After
+//    public void clearStreams() {
+//        //clear for future usage
+//        System.setOut(originalOut);
+//        System.setErr(originalErr);
+//    }
+//    @Test
+//    public void testDriverLogin() throws Exception {
+//        //create a test user
+//        //try to log into test user
+//        //check if prints ok
+//        //Driver testDriver = new Driver();
+//
+//    }
     @Test
     public void testDriverLogin_driverNotFound() throws Exception {
         String testName = RandomStringUtils.randomAlphabetic(7);
