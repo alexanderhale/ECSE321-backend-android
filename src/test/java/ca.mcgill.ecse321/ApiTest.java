c7a9a72a9136012f1fc1a874e39117458f66ab18
eozer <ege.ozer@mail.mcgill.ca>
2018-10-07 23:35:44 -0400

    Added unit tests for Login for both Driver and Rider

--- src/test/java/ca.mcgill.ecse321/ApiTest.java
+++ src/test/java/ca.mcgill.ecse321/ApiTest.java
@@ -1,9 +1,7 @@
 package ca.mcgill.ecse321;
 
 import ca.mcgill.ecse321.driver.Driver;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.lang3.RandomStringUtils;
-import org.apache.commons.lang3.RandomUtils;
 import org.apache.http.HttpHeaders;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
@@ -15,10 +13,8 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 
 import java.io.IOException;
 
@@ -28,16 +24,6 @@ import static org.junit.Assert.assertEquals;
 @RunWith(SpringRunner.class)
 @WebMvcTest(Driver.class)
 public class ApiTest {
-
-    @Autowired
-    private MockMvc mvc;
-    @Autowired
-    private ObjectMapper mapper;
-
-    private Driver someClass;  //this could be Autowired
-
-    //, initialized in the test method
-    //, or created in setup block
 
     @Test // test whether the application endpoint is secure
     public void testSecureEndpoint() throws IOException {
@@ -55,20 +41,6 @@ public class ApiTest {
         assertEquals(jSonType, type);
     }
 
-    @Test // test what happens when you call a Driver without any other info
-    public void testDriverIsNull() throws IOException {
-        HttpUriRequest request = new HttpGet("https://ecse321-project.herokuapp.com/driver");
-        HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
-        assertEquals(httpResponse.getStatusLine().getStatusCode(), HttpStatus.SC_NOT_FOUND);
-    }
-
-    @Test // test what happens when you call a Rider without any other info
-    public void testRiderIsNull() throws IOException {
-        HttpUriRequest request = new HttpGet("https://ecse321-project.herokuapp.com/rider");
-        HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
-        assertEquals(httpResponse.getStatusLine().getStatusCode(), HttpStatus.SC_NOT_FOUND);
-    }
-
     @Test // test that a Driver registration is rejected if the username is already in use
     public void testDriverRegisterDuplicate() throws Exception {
 
@@ -77,31 +49,15 @@ public class ApiTest {
 
     @Test // test that a Rider registration is rejected if the username is already in use
     public void testRiderRegisterDuplicate() throws Exception {
-        String testName = RandomStringUtils.randomAlphabetic(7);
-        HttpUriRequest request = new HttpGet("https://ecse321-project.herokuapp.com/rider/register" + testName);
-        HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
-        assertEquals(httpResponse.getStatusLine().getStatusCode(), HttpStatus.SC_CONFLICT);
-    }
 
-    @Test // test that a Driver can be successfully registered when the parameters are correct
-    public void testDriverRegisterSuccessfully() throws Exception {
-        HttpUriRequest request = new HttpPost("https://ecse321-project.herokuapp.com/driver/login");
-
-
-        StringEntity input = new StringEntity("{\"username\":\"ege\",\n" +
-                "\t\"password\":\"pass\"\n" +
-                "}");
-        ((HttpPost) request).setEntity(input);
-        request.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
-        HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
-        assertEquals(HttpStatus.SC_OK, httpResponse.getStatusLine().getStatusCode());
     }
 
 
     @Test // test that a Driver login is rejected if the username is null
     public void testDriverLoginNullUser() throws Exception {
         HttpUriRequest request = new HttpPost("https://ecse321-project.herokuapp.com/driver/login");
-        StringEntity input = new StringEntity("{\"username\":\"\",\n" +
+        StringEntity input = new StringEntity("{\"username\":\"" + null +
+                "\",\n" +
                 "\t\"password\":\"pass\"\n" +
                 "}");
         ((HttpPost) request).setEntity(input);
@@ -114,7 +70,8 @@ public class ApiTest {
     public void testDriverLoginNullPassword() throws Exception {
         HttpUriRequest request = new HttpPost("https://ecse321-project.herokuapp.com/driver/login");
         StringEntity input = new StringEntity("{\"username\":\"ege\",\n" +
-                "\t\"password\":\"\"\n" +
+                "\t\"password\":\"" + null +
+                "\"\n" +
                 "}");
         ((HttpPost) request).setEntity(input);
         request.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
@@ -123,67 +80,124 @@ public class ApiTest {
     }
 
     @Test // test what happens when you try to log in with a Driver that is not found
-    public void testDriverLogin_driverNotFound() throws Exception {
-        String testName = RandomStringUtils.randomAlphabetic(7);
-        System.out.println(testName + "ss");
-        HttpUriRequest request = new HttpGet("https://ecse321-project.herokuapp.com/driver" + testName);
+    public void testDriverLoginNonExistentDriver() throws Exception {
+        String nonExistentDriverName = RandomStringUtils.randomAlphabetic(7);
+        HttpUriRequest request = new HttpPost("https://ecse321-project.herokuapp.com/driver/login");
+        StringEntity input = new StringEntity("{\"username\":\"" + nonExistentDriverName +
+                "\",\n" +
+                "\t\"password\":\"\"\n" +
+                "}");
+        ((HttpPost) request).setEntity(input);
+        request.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
         HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
-        assertEquals(httpResponse.getStatusLine().getStatusCode(), HttpStatus.SC_NOT_FOUND);
+        assertEquals(HttpStatus.SC_INTERNAL_SERVER_ERROR, httpResponse.getStatusLine().getStatusCode());
     }
 
     @Test   // test that a Driver login is rejected if the password is wrong
     public void testDriverLoginWrongPassword() throws Exception {
-        // TODO
+        String wrongPass = RandomStringUtils.randomAlphabetic(7);
+        HttpUriRequest request = new HttpPost("https://ecse321-project.herokuapp.com/driver/login");
+        StringEntity input = new StringEntity("{\"username\":\"ege\",\n" +
+                "\t\"password\":\"" + wrongPass +
+                "\"\n" +
+                "}");
+        ((HttpPost) request).setEntity(input);
+        request.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
+        HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
+        assertEquals(HttpStatus.SC_INTERNAL_SERVER_ERROR, httpResponse.getStatusLine().getStatusCode());
     }
 
     @Test //test that a Rider login is rejected if the username is null
     public void testRiderLoginNullUser() throws Exception {
-        String testName = "";
-        HttpUriRequest request = new HttpGet("https://ecse321-project.herokuapp.com/rider/login" + testName);
+        HttpUriRequest request = new HttpPost("https://ecse321-project.herokuapp.com/rider/login");
+        StringEntity input = new StringEntity("{\"username\":\"" + null +
+                "\",\n" +
+                "\t\"password\":\"pass\"\n" +
+                "}");
+        ((HttpPost) request).setEntity(input);
+        request.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
         HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
-        assertEquals(httpResponse.getStatusLine().getStatusCode(), HttpStatus.SC_BAD_REQUEST);
+        assertEquals(HttpStatus.SC_INTERNAL_SERVER_ERROR, httpResponse.getStatusLine().getStatusCode());
     }
 
     @Test   // test that a Rider login is rejected if the password is null
     public void testRiderLoginNullPassword() throws Exception {
-        // TODO
+        HttpUriRequest request = new HttpPost("https://ecse321-project.herokuapp.com/rider/login");
+        StringEntity input = new StringEntity("{\"username\":\"ege\",\n" +
+                "\t\"password\":\"" + null +
+                "\"\n" +
+                "}");
+        ((HttpPost) request).setEntity(input);
+        request.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
+        HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
+        assertEquals(HttpStatus.SC_INTERNAL_SERVER_ERROR, httpResponse.getStatusLine().getStatusCode());
     }
 
     @Test   // test that a Rider login is rejected if the password is wrong
     public void testRiderLoginWrongPassword() throws Exception {
-        // TODO
+        String wrongPass = RandomStringUtils.randomAlphabetic(7);
+        HttpUriRequest request = new HttpPost("https://ecse321-project.herokuapp.com/rider/login");
+        StringEntity input = new StringEntity("{\"username\":\"elon\",\n" +
+                "\t\"password\":\"" + wrongPass +
+                "\"\n" +
+                "}");
+        ((HttpPost) request).setEntity(input);
+        request.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
+        HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
+        assertEquals(HttpStatus.SC_INTERNAL_SERVER_ERROR, httpResponse.getStatusLine().getStatusCode());
     }
 
-    @Test // test what happens when you try to log in with a Rider that is not found
-    public void testRiderLogin_driverNotFound() throws Exception {
-        String testName = RandomStringUtils.randomAlphabetic(7);
-        System.out.println(testName);
-        HttpUriRequest request = new HttpGet("https://ecse321-project.herokuapp.com/rider" + testName);
+    @Test // test what happens when you try to log in with a Driver that is not found
+    public void testRiderLoginNonExistentRider() throws Exception {
+        String nonExistentRiderName = RandomStringUtils.randomAlphabetic(7);
+        HttpUriRequest request = new HttpPost("https://ecse321-project.herokuapp.com/rider/login");
+        StringEntity input = new StringEntity("{\"username\":\"" + nonExistentRiderName +
+                "\",\n" +
+                "\t\"password\":\"\"\n" +
+                "}");
+        ((HttpPost) request).setEntity(input);
+        request.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
         HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
-        assertEquals(httpResponse.getStatusLine().getStatusCode(), HttpStatus.SC_NOT_FOUND);
+        assertEquals(HttpStatus.SC_INTERNAL_SERVER_ERROR, httpResponse.getStatusLine().getStatusCode());
     }
 
     @Test   // test that a Driver can be successfully logged in when the parameters are correct
     public void testDriverLoginSuccess() throws Exception {
-        // TODO
+        HttpUriRequest request = new HttpPost("https://ecse321-project.herokuapp.com/driver/login");
+        StringEntity input = new StringEntity("{\"username\":\"ege\",\n" +
+                "\t\"password\":\"pass\"\n" +
+                "}");
+        ((HttpPost) request).setEntity(input);
+        request.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
+        HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
+        assertEquals(HttpStatus.SC_OK, httpResponse.getStatusLine().getStatusCode());
     }
 
     @Test   // test that a Rider can be successfully logged in when the parameters are correct
     public void testRiderLoginSuccess() throws Exception {
-        // TODO
+        HttpUriRequest request = new HttpPost("https://ecse321-project.herokuapp.com/rider/login");
+
+
+        StringEntity input = new StringEntity("{\"username\":\"elon\",\n" +
+                "\t\"password\":\"musk\"\n" +
+                "}");
+        ((HttpPost) request).setEntity(input);
+        request.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
+        HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
+        assertEquals(HttpStatus.SC_OK, httpResponse.getStatusLine().getStatusCode());
     }
 
     @Test   // test that all Drivers are returned properly
     public void getAllDriversSuccessfully() throws Exception {
         HttpUriRequest request = new HttpGet("https://ecse321-project.herokuapp.com/driver/secure/all");
         HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
-        assertEquals(httpResponse.getStatusLine().getStatusCode(), HttpStatus.SC_OK);
+        assertEquals(HttpStatus.SC_OK, httpResponse.getStatusLine().getStatusCode());
     }
 
     @Test // test that all Riders are returned properly
     public void getAllRidersSuccessfully() throws Exception {
         HttpUriRequest request = new HttpGet("https://ecse321-project.herokuapp.com/rider/secure/all");
         HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
-        assertEquals(httpResponse.getStatusLine().getStatusCode(), HttpStatus.SC_OK);
+        assertEquals(HttpStatus.SC_OK, httpResponse.getStatusLine().getStatusCode());
     }
 }
