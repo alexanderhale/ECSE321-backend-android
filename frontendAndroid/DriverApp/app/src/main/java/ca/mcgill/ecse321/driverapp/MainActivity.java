c3505cef81c4a66dfd568300ef0d8b71d4c47f3a
Walid Chabchoub <walid.chabchoub@mail.mcgill.ca>
2018-10-25 18:18:54 -0400

    Added view layouts and refactored driver folder

--- frontendAndroid/DriverApp/app/src/main/java/ca/mcgill/ecse321/driverapp/MainActivity.java
+++ frontendAndroid/DriverApp/app/src/main/java/ca/mcgill/ecse321/driverapp/MainActivity.java
@@ -1,4 +1,4 @@
-package ca.mcgill.ecse321.driver;
+package ca.mcgill.ecse321.driverapp;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,18 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
-
-import com.loopj.android.http.JsonHttpResponseHandler;
-import com.loopj.android.http.RequestParams;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
-    private String error = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +26,6 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-        refreshErrorMessage();
     }
 
     @Override
@@ -59,39 +48,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void addParticipant(View v) {
-        error = "";
-        final TextView tv = (TextView) findViewById(R.id.newparticipant_name);
-        HttpUtils.post("participants/" + tv.getText().toString(), new RequestParams(), new JsonHttpResponseHandler() {
-            @Override
-            public void onFinish() {
-                refreshErrorMessage();
-                tv.setText("");
-            }
-            @Override
-            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
-                try {
-                    error += errorResponse.get("message").toString();
-                } catch (JSONException e) {
-                    error += e.getMessage();
-                }
-                refreshErrorMessage();
-            }
-        });
-    }
-
-    private void refreshErrorMessage() {
-        // set the error message
-        TextView tvError = (TextView) findViewById(R.id.error);
-        tvError.setText(error);
-
-        if (error == null || error.length() == 0) {
-            tvError.setVisibility(View.GONE);
-        } else {
-            tvError.setVisibility(View.VISIBLE);
-        }
-
     }
 }
