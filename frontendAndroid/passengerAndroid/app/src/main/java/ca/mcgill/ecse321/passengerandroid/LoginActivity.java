12f145f6e6849bf002b5e9543d8769b460f509b1
Ege <ege.ozer@mail.mcgill.ca>
2018-10-28 23:04:17 -0400

    Added Register page/activity for passenger

--- frontendAndroid/passengerAndroid/app/src/main/java/ca/mcgill/ecse321/passengerandroid/LoginActivity.java
+++ frontendAndroid/passengerAndroid/app/src/main/java/ca/mcgill/ecse321/passengerandroid/LoginActivity.java
@@ -30,21 +30,7 @@ public class LoginActivity extends AppCompatActivity {
         error = "";
         final TextView tv = (TextView) findViewById(R.id.usernameField);
         System.out.println(tv.getText());
-//        HttpUtils.post("username/" + tv.getText().toString(), new RequestParams(), new JsonHttpResponseHandler() {
-//            @Override
-//            public void onFinish() {
-//                tv.setText("");
-//            }
-//
-//            @Override
-//            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
-//                try {
-//                    error += errorResponse.get("message").toString();
-//                } catch (JSONException e) {
-//                    error += e.getMessage();
-//                }
-//            }
-//        });
+
     }
 
 
