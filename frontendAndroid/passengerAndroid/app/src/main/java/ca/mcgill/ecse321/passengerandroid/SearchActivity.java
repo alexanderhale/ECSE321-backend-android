f89f92873e6a837d9de15136acfbd41fbc1e223b
alexhale <a.hale@live.ca>
2018-10-31 17:53:21 -0400

    started files for passengerAndroid search activity

---
+++ frontendAndroid/passengerAndroid/app/src/main/java/ca/mcgill/ecse321/passengerandroid/SearchActivity.java
@@ -0,0 +1,31 @@
+package ca.mcgill.ecse321.passengerandroid;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+public class SearchActivity extends AppCompatActivity {
+    private String error = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+    }
+
+    protected void sortByDriver(View view) {
+        // TODO
+
+    }
+
+    protected void sortByCarModel(View view) {
+        // TODO
+    }
+
+    protected void sortByPrice(View view) {
+        // TODO
+    }
+
+}
