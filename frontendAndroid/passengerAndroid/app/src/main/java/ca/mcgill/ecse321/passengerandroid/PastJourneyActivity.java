01f2b9c06742b0d9dc4920aaee4710e9713b6d76
halestorm962 <a.hale@live.ca>
2018-10-31 20:02:39 -0400

    attempted passengerApp.searchActivity layout

---
+++ frontendAndroid/passengerAndroid/app/src/main/java/ca/mcgill/ecse321/passengerandroid/PastJourneyActivity.java
@@ -0,0 +1,19 @@
+package ca.mcgill.ecse321.passengerandroid;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class PastJourneyActivity extends AppCompatActivity {
+    private String error = null;
+
+    /*@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }*/
+
+}
